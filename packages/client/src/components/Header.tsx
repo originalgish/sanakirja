405d873ead99d91cb539c659ed5563e35f3c38a3
Denis Magazeishchikov <gish48@gmail.com>
2023-03-01 22:35:11 +0200

    select mode UI

--- packages/client/src/components/Header.tsx
+++ packages/client/src/components/Header.tsx
@@ -1,24 +1,18 @@
+import { MenuOutlined } from "@ant-design/icons";
 import { Button, Typography } from "antd";
-import useSWRMutation from "swr/mutation";
 
-const fetcher = (url: string) =>
-  fetch(url, {
-    method: "post",
-  });
-const BASE_URL = `${process.env.REACT_APP_API_URL}/api/v1/words`;
+type Props = {
+  onDrawerOpen: () => void;
+};
 
-export const Header = () => {
-  const { trigger, isMutating } = useSWRMutation(`${BASE_URL}/load`, fetcher);
-
+export const Header = ({ onDrawerOpen }: Props) => {
   return (
     <header className="header">
       <Typography.Title level={2} style={{ margin: 0 }}>
         Sanakirja
       </Typography.Title>
 
-      <Button type="default" onClick={trigger} loading={isMutating}>
-        Sync words
-      </Button>
+      <Button type="text" icon={<MenuOutlined />} onClick={onDrawerOpen}></Button>
     </header>
   );
 };
--- packages/client/src/components/index.ts
+++ packages/client/src/components/index.ts
@@ -1,3 +1,4 @@
 export { Header } from "./Header";
 export { Card } from "./Card";
 export { Word } from "./Word";
+export { Drawer } from "./Drawer";
