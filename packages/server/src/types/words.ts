232cf5e93d03cb3860476f4277356d574c956acd
Denis Magazeishchikov <gish48@gmail.com>
2023-03-01 23:13:10 +0200

    set mode server

--- packages/server/src/types/words.ts
+++ packages/server/src/types/words.ts
@@ -1,8 +1,10 @@
-export type Word = {
-  finnish: string;
-  english: string;
-};
+export type Languages = "finnish" | "english";
+
+export type Mode = Languages;
+
+export type Word = Record<Languages, string>;
 
 export type Words = {
+  mode: Mode;
   words: Word[];
 };
