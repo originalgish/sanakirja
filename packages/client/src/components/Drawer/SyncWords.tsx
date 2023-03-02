8ae5673a7bbc6fdc635866245c4a72d48851e933
Denis Magazeishchikov <gish48@gmail.com>
2023-03-02 21:48:45 +0200

    client mode api

---
+++ packages/client/src/components/Drawer/SyncWords.tsx
@@ -0,0 +1,16 @@
+import { Button } from "antd";
+import useSWRMutation from "swr/mutation";
+
+import { api } from "api";
+
+const syncWords = () => api.put("/words/load");
+
+export const SyncWords = () => {
+  const { trigger, isMutating } = useSWRMutation("syncWords", syncWords);
+
+  return (
+    <Button type="default" htmlType="button" onClick={trigger} loading={isMutating}>
+      Sync words
+    </Button>
+  );
+};
