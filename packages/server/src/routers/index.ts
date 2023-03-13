000df1f2bba66692f92d3cb3fd78c3dcc938b516
Denis Magazeishchikov <gish48@gmail.com>
2023-03-13 16:25:14 +0200

    server mode to user preferences

--- packages/server/src/routers/index.ts
+++ packages/server/src/routers/index.ts
@@ -1,3 +1,2 @@
 export { router as wordsRouter } from "./words";
-export { router as settingsRouter } from "./settings";
 export { router as userRouter } from "./user";
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,9 +2,14 @@ export const languages = ["finnish", "english"] as const;
 
 export type Language = (typeof languages)[number];
 
+export type UserPreferences = {
+  mode: Language;
+};
+
 export type User = {
   name: string;
   password: string;
+  preferences: UserPreferences;
   tokens: {
     token: string;
   }[];
