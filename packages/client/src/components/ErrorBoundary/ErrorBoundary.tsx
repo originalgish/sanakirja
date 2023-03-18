4dc3f985a56c8624c5913fc7051657caedd9802c
Denis Magazeishchikov <gish48@gmail.com>
2023-03-18 14:41:04 +0200

    added error boundary

---
+++ packages/client/src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,34 @@
+import { Component } from "react";
+import type { ErrorInfo, ReactNode } from "react";
+
+import { ErrorComponent } from "./ErrorComponent";
+
+interface Props {
+  children?: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+export class ErrorBoundary extends Component<Props, State> {
+  public state: State = {
+    hasError: false,
+  };
+
+  public static getDerivedStateFromError(): State {
+    return { hasError: true };
+  }
+
+  public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error("Uncaught error:", error, errorInfo);
+  }
+
+  public render() {
+    if (this.state.hasError) {
+      return <ErrorComponent />;
+    }
+
+    return this.props.children;
+  }
+}
--- packages/client/src/components/index.ts
+++ packages/client/src/components/index.ts
@@ -1,3 +1,5 @@
+export { ErrorBoundary } from "./ErrorBoundary";
+
 export { Header } from "./Header";
 export { Card } from "./Card";
 export { Word } from "./Word";
