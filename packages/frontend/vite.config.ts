ff065c0d9b3e7508022cb0562d4c95c8fbc19e3f
Denis Magazeishchikov <gish48@gmail.com>
2023-03-19 20:42:44 +0200

    migrated src to vite

---
+++ packages/frontend/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    proxy: {
+      "/api": "http://localhost:3001",
+    },
+  },
+  optimizeDeps: {
+    include: ["@sanakirja/shared"],
+  },
+  build: {
+    commonjsOptions: {
+      include: [/@sanakirja\/shared/, /node_modules/],
+    },
+  },
+});
