c065fd9c6bfdb101a355dd565c9f16ef4c5e9628
Denis Magazeishchikov <gish48@gmail.com>
2023-03-19 21:51:28 +0200

    fixed testing

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -23,5 +24,10 @@ export default defineConfig({
         },
       },
     },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
   },
 });
